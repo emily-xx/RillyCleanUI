fabbbbe46fac09ff7e0e16aeaeb1efb140c78b6a
Emma Chatley <emchatley@gmail.com>
2019-10-16 20:55:56 +0100

    Colouring out of range red

--- AddOns/RillyCleanUI/src/ActionBars.lua
+++ AddOns/RillyCleanUI/src/ActionBars.lua
@@ -108,6 +108,44 @@ if not dominos and not bartender4 then
 
   -- Store button needs moved off screen as it does not have Hide() for some reason.
   StoreMicroButton:SetPoint("TOPLEFT",-250,-50000)
+
+  -- Color out of range red
+  local CF=CreateFrame("Frame")
+  CF:RegisterEvent("ADDON_LOADED")
+  CF:SetScript("OnEvent", function(self, event)
+  	hooksecurefunc(
+  	    "ActionButton_OnEvent",
+  	    function(self, event, ...)
+  	        if (event == "PLAYER_TARGET_CHANGED") then
+  	            self.newTimer = self.rangeTimer
+  	        end
+  	    end
+  	)
+  	hooksecurefunc(
+  	    "ActionButton_UpdateUsable",
+  	    function(self)
+  	        local icon = _G[self:GetName() .. "Icon"]
+  	        local valid = IsActionInRange(self.action)
+  	        if (valid == false) then
+  	            icon:SetVertexColor(1, 0.2, 0.1)
+  	        end
+  	    end
+  	)
+  	hooksecurefunc(
+  	    "ActionButton_OnUpdate",
+  	    function(self, elapsed)
+  	        local rangeTimer = self.newTimer
+  	        if (rangeTimer) then
+  	            rangeTimer = rangeTimer - elapsed
+  	            if (rangeTimer <= 0) then
+  	                ActionButton_UpdateUsable(self)
+  	                rangeTimer = TOOLTIP_UPDATE_TIME
+  	            end
+  	            self.newTimer = rangeTimer
+  	        end
+  	    end
+  	)
+  end)
 end
 
 --backdrop settings
