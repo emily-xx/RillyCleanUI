4ea488f0a3c151d7341bc784e3417403c7287243
Emma Chatley <emchatley@gmail.com>
2020-04-10 14:30:22 +0100

    Fixed out of range coloring

--- AddOns/RillyCleanUI/src/ActionBars.lua
+++ AddOns/RillyCleanUI/src/ActionBars.lua
@@ -122,42 +122,38 @@ local function init()
     StoreMicroButton:SetPoint("TOPLEFT",-250,-50000)
 
     -- Color out of range red
-    local CF=CreateFrame("Frame")
-    CF:RegisterEvent("ADDON_LOADED")
-    CF:SetScript("OnEvent", function(self, event)
-      hooksecurefunc(
-          "ActionButton_OnEvent",
-          function(self, event, ...)
-              if (event == "PLAYER_TARGET_CHANGED") then
-                  self.newTimer = self.rangeTimer
-              end
-          end
-      )
-      hooksecurefunc(
-          "ActionButton_UpdateUsable",
-          function(self)
-              local icon = _G[self:GetName() .. "Icon"]
-              local valid = IsActionInRange(self.action)
-              if (valid == false) then
-                  icon:SetVertexColor(1, 0.2, 0.1)
-              end
-          end
-      )
-      hooksecurefunc(
-          "ActionButton_OnUpdate",
-          function(self, elapsed)
-              local rangeTimer = self.newTimer
-              if (rangeTimer) then
-                  rangeTimer = rangeTimer - elapsed
-                  if (rangeTimer <= 0) then
-                      ActionButton_UpdateUsable(self)
-                      rangeTimer = TOOLTIP_UPDATE_TIME
-                  end
-                  self.newTimer = rangeTimer
-              end
-          end
-      )
-    end)
+    hooksecurefunc(
+        "ActionButton_OnEvent",
+        function(self, event, ...)
+            if (event == "PLAYER_TARGET_CHANGED") then
+                self.newTimer = self.rangeTimer
+            end
+        end
+    )
+    hooksecurefunc(
+        "ActionButton_UpdateUsable",
+        function(self)
+            local icon = _G[self:GetName() .. "Icon"]
+            local valid = IsActionInRange(self.action)
+            if (valid == false) then
+                icon:SetVertexColor(1, 0.2, 0.1)
+            end
+        end
+    )
+    hooksecurefunc(
+        "ActionButton_OnUpdate",
+        function(self, elapsed)
+            local rangeTimer = self.newTimer
+            if (rangeTimer) then
+                rangeTimer = rangeTimer - elapsed
+                if (rangeTimer <= 0) then
+                    ActionButton_UpdateUsable(self)
+                    rangeTimer = TOOLTIP_UPDATE_TIME
+                end
+                self.newTimer = rangeTimer
+            end
+        end
+    )
   end
 
   --backdrop settings
