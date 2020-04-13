3319416e61c702598d414e43f45d062924c2f2ed
Emma Chatley <emchatley@gmail.com>
2020-04-13 03:31:15 +0100

    Fixed DamageFont loading

--- AddOns/RillyCleanUI/src/DamageFont.lua
+++ AddOns/RillyCleanUI/src/DamageFont.lua
@@ -1,12 +1,10 @@
 RillyNiceDamage = CreateFrame("Frame", "RillyNiceDamage")
 
-local damagefont_FONT_NUMBER = "Interface\\AddOns\\RillyCleanUI\\fonts\\ZCOOLKuaiLe-Regular.ttf"
-
 function RillyNiceDamage:ApplySystemFonts()
-  DAMAGE_TEXT_FONT = damagefont_FONT_NUMBER
+  DAMAGE_TEXT_FONT = "Interface\\AddOns\\RillyCleanUI\\fonts\\ZCOOLKuaiLe-Regular.ttf"
 end
 
-RillyNiceDamage:RegisterEvent("PLAYER_LOGIN")
+RillyNiceDamage:RegisterEvent("ADDON_LOADED")
 
 RillyNiceDamage:SetScript("OnEvent", function()
   if (RCUIDB.damageFont) then
