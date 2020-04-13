7f79d73ffc7652a6f8e5cb3edb3dad22362626f4
Emma Chatley <emchatley@gmail.com>
2020-04-13 03:21:48 +0100

    Spaces

--- AddOns/RillyCleanUI/src/DamageFont.lua
+++ AddOns/RillyCleanUI/src/DamageFont.lua
@@ -3,13 +3,13 @@ RillyNiceDamage = CreateFrame("Frame", "RillyNiceDamage")
 local damagefont_FONT_NUMBER = "Interface\\AddOns\\RillyCleanUI\\fonts\\ZCOOLKuaiLe-Regular.ttf"
 
 function RillyNiceDamage:ApplySystemFonts()
-	DAMAGE_TEXT_FONT = damagefont_FONT_NUMBER
+  DAMAGE_TEXT_FONT = damagefont_FONT_NUMBER
 end
 
 RillyNiceDamage:RegisterEvent("PLAYER_LOGIN")
 
 RillyNiceDamage:SetScript("OnEvent", function()
-	if (RCUIDB.damageFont) then
-		RillyNiceDamage:ApplySystemFonts()
-	end
+  if (RCUIDB.damageFont) then
+    RillyNiceDamage:ApplySystemFonts()
+  end
 end)
