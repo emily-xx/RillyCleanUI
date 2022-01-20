becddf066c2a8501c12fe70c166141c71d721461
Emma Chatley <emchatley@gmail.com>
2022-01-20 14:27:03 +0000

    Fix bug with missing CAST_BACKDROP

--- Interface/AddOns/RillyCleanUI/src/Common.lua
+++ Interface/AddOns/RillyCleanUI/src/Common.lua
@@ -14,6 +14,20 @@ RILLY_CLEAN_BACKDROP = {
   },
 }
 
+CAST_BACKDROP = {
+  bgFile = nil,
+  edgeFile = SQUARE_TEXTURE,
+  tile = false,
+  tileSize = 32,
+  edgeSize = 2,
+  insets = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0
+  },
+}
+
 function skinNineSlice(ns)
   local nsPoints = {
     "TopLeftCorner",
