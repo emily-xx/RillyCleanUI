940d87f811ed730e25746c6a043c2f7f7e74cb9b
Emma Chatley <emchatley@gmail.com>
2022-02-20 13:13:48 +0000

    Consolidate BigDebuffs skinning code

--- Interface/AddOns/RillyCleanUI/src/BigDebuffsSkin.lua
+++ Interface/AddOns/RillyCleanUI/src/BigDebuffsSkin.lua
@@ -2,17 +2,33 @@ RillyCleanBigDebuffs = CreateFrame("Frame", "RillyCleanBigDebuffs")
 RillyCleanBigDebuffs:RegisterEvent("PLAYER_LOGIN")
 
 RillyCleanBigDebuffs:SetScript("OnEvent", function()
-  if (IsAddOnLoaded('BigDebuffs')) then
-    hooksecurefunc(BigDebuffs, 'NAME_PLATE_UNIT_ADDED', function(self, _, unit)
-      local namePlate = C_NamePlate.GetNamePlateForUnit(unit)
-      if (namePlate:IsForbidden()) then return end
+  if ( not IsAddOnLoaded('BigDebuffs') ) then return end
 
-      local bdbFrame = namePlate.UnitFrame
-      local bdbNameplate = bdbFrame.BigDebuffs
+  -- Unit portraits
+  hooksecurefunc(BigDebuffs, 'AttachUnitFrame', function(self, unit)
+    local frameName = 'BigDebuffs' .. unit .. "UnitFrame"
+    local bdbFrame = _G[frameName]
 
-      if (bdbNameplate) then
-        applyRillyCleanButtonSkin(bdbNameplate)
-      end
-    end)
-  end
+    local icon = bdbFrame.icon
+
+    if (icon) then
+      local cooldown = bdbFrame.cooldown or bdbFrame.Cooldown
+      icon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
+      cooldown:SetAllPoints(icon)
+      cooldown:SetSwipeTexture(SQUARE_TEXTURE)
+    end
+  end)
+
+  -- Nameplates
+  hooksecurefunc(BigDebuffs, 'NAME_PLATE_UNIT_ADDED', function(self, _, unit)
+    local namePlate = C_NamePlate.GetNamePlateForUnit(unit)
+    if (namePlate:IsForbidden()) then return end
+
+    local bdbFrame = namePlate.UnitFrame
+    local bdbNameplate = bdbFrame.BigDebuffs
+
+    if (bdbNameplate) then
+      applyRillyCleanButtonSkin(bdbNameplate)
+    end
+  end)
 end)
