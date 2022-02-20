34272017dacbbe54e28dafe065a0bed71dae220d
Emma Chatley <emchatley@gmail.com>
2022-02-20 04:47:59 +0000

    Cleanup excess indentation

--- Interface/AddOns/RillyCleanUI/src/Portrait.lua
+++ Interface/AddOns/RillyCleanUI/src/Portrait.lua
@@ -9,7 +9,7 @@ end
 
 local function makePortraitBG(frame, r, g, b)
   frame.portraitBG = CreateFrame("Frame", nil, frame, "BackdropTemplate")
-  frame.portraitBG:SetFrameLevel(0)
+  frame.portraitBG:SetFrameLevel(frame:GetFrameLevel() - 1)
   frame.portraitBG:SetFrameStrata("background")
   frame.portraitBG:SetAllPoints(frame.portrait)
 
@@ -24,55 +24,56 @@ local CF=CreateFrame("Frame")
 CF:RegisterEvent("PLAYER_LOGIN")
 CF:SetScript("OnEvent", function(self, event)
 hooksecurefunc("UnitFramePortrait_Update",function(frame)
-  if frame.portrait then
-    if( RCUIDB.portraitStyle == "class" ) then -- Flat class icons
-      if( UnitIsPlayer(frame.unit) ) then
-        local t = CLASS_ICON_TCOORDS[select(2, UnitClass(frame.unit))]
-        if t then
-          frame.portrait:SetTexture("Interface\\TargetingFrame\\UI-Classes-Circles")
-          frame.portrait:SetTexCoord(unpack(t))
-          makePortraitBG(frame,0.05,0.05,0.05)
-        end
-      else
-        frame.portrait:SetTexCoord(0.15, 0.85, 0.15, 0.85)
-      end
-    elseif( RCUIDB.portraitStyle == "2D" ) then -- Standard 2D character image, but made square
-      frame.portrait:SetTexCoord(0.15, 0.85, 0.15, 0.85)
-    elseif (RCUIDB.portraitStyle == "3D") then
-      if ( not frame.portraitModel ) then -- Initialize 3D Model Container
-        local portraitModel = CreateFrame("PlayerModel", nil, frame)
-        portraitModel:SetScript("OnShow", resetCamera)
-        portraitModel:SetScript("OnHide", resetGUID)
-        portraitModel.parent = frame
-        portraitModel:SetFrameLevel(frame:GetFrameLevel())
+  if not frame.portrait then return end
 
+  if ( RCUIDB.portraitStyle == "class" ) then -- Flat class icons
+    if ( UnitIsPlayer(frame.unit) ) then
+      local t = CLASS_ICON_TCOORDS[select(2, UnitClass(frame.unit))]
+      if t then
+        frame.portrait:SetTexture("Interface\\TargetingFrame\\UI-Classes-Circles")
+        frame.portrait:SetTexCoord(unpack(t))
         makePortraitBG(frame,0.05,0.05,0.05)
-
-        portraitModel:SetAllPoints(frame.portrait)
-        portraitModel:Show()
-        frame.portrait:Hide()
-        frame.portraitModel = portraitModel
       end
-
-      -- Portrait models can't be updated unless the GUID changed or else you have the animation jumping around
-      frame.portraitModel.guid = UnitGUID(frame.unit)
-
-      -- The players not in range so swap to question mark
-      if ( not UnitIsVisible(frame.unit) or not UnitIsConnected(frame.unit) ) then
-        frame.portraitModel:ClearModel()
-        frame.portraitModel:SetModelScale(5.5)
-        resetCamera(frame.portraitModel)
-        frame.portraitModel:SetModel("Interface\\Buttons\\talktomequestionmark.m2")
-      -- Use animated 3D portrait
-      else
-        frame.portraitModel:ClearModel()
-        frame.portraitModel:SetUnit(frame.unit)
-        resetCamera(frame.portraitModel)
-        frame.portraitModel:SetPosition(0, 0, 0)
-        frame.portraitModel:SetAnimation(804)
-        frame.portraitModel:Show()
-      end
+    else
+      frame.portrait:SetTexCoord(0.15, 0.85, 0.15, 0.85)
     end
+  elseif (RCUIDB.portraitStyle == "3D") then
+    if ( not frame.portraitModel ) then -- Initialize 3D Model Container
+      local portraitModel = CreateFrame("PlayerModel", nil, frame)
+      portraitModel:SetScript("OnShow", resetCamera)
+      portraitModel:SetScript("OnHide", resetGUID)
+      portraitModel.parent = frame
+      portraitModel:SetFrameLevel(frame:GetFrameLevel())
+
+      makePortraitBG(frame,0.05,0.05,0.05)
+
+      portraitModel:SetAllPoints(frame.portrait)
+      portraitModel:Show()
+      frame.portrait:Hide()
+      frame.portraitModel = portraitModel
     end
+
+    -- Portrait models can't be updated unless the GUID changed or else you have the animation jumping around
+    frame.portraitModel.guid = UnitGUID(frame.unit)
+
+    -- The players not in range so swap to question mark
+    if ( not UnitIsVisible(frame.unit) or not UnitIsConnected(frame.unit) ) then
+      frame.portraitModel:ClearModel()
+      frame.portraitModel:SetModelScale(5.5)
+      resetCamera(frame.portraitModel)
+      frame.portraitModel:SetModel("Interface\\Buttons\\talktomequestionmark.m2")
+    -- Use animated 3D portrait
+    else
+      frame.portraitModel:ClearModel()
+      frame.portraitModel:SetModelScale(1)
+      frame.portraitModel:SetUnit(frame.unit)
+      resetCamera(frame.portraitModel)
+      frame.portraitModel:SetPosition(0, 0, 0)
+      frame.portraitModel:SetAnimation(804)
+      frame.portraitModel:Show()
+    end
+  else -- Standard 2D character image, but made square
+    frame.portrait:SetTexCoord(0.15, 0.85, 0.15, 0.85)
+  end
   end)
 end)
