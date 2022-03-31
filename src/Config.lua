691ccfa114f7611f9f79675c94c3e4be1425d2dc
Emma Chatley <emchatley@gmail.com>
2022-03-31 15:33:24 +0100

    Ensure nameplate width settings are not randomly reverted when existing interface options

--- src/Config.lua
+++ src/Config.lua
@@ -569,6 +569,13 @@ local function rcui_options()
   SlashCmdList["rcui"] = function()
     openRcuiConfig()
   end
+
+  hooksecurefunc("OptionsFrame_OnHide", function(self) -- Ensure some settings are not randomly reverted when closing/saving Interface Options
+    local frameName = self:GetName()
+    if frameName ~= 'InterfaceOptionsFrame' then return end
+
+    SetFriendlyNameplateSize()
+  end)
 end
 
 GameMenuFrame.Header:Hide()
