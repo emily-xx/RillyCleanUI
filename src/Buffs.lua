f23777032dce70f8c2dd1f0733a2336a37c5dac7
Emma Chatley <emchatley@gmail.com>
2022-10-27 22:51:31 +0100

    - Fix and greatly simplify Buff code. - Add new Aura Border texture - Set colour for debuff type from aura border and remove default border

--- src/Buffs.lua
+++ src/Buffs.lua
@@ -1,24 +1,8 @@
 local function applySkin(b)
   if not b or (b and b.rillyClean) then return end
 
-  local name = b:GetName()
-
-  local tempenchant, consolidated, debuff, buff = false, false, false, false
-  if (name:match("TempEnchant")) then
-    tempenchant = true
-  elseif (name:match("Consolidated")) then
-    consolidated = true
-  elseif (name:match("Debuff")) then
-    debuff = true
-  else
-    buff = true
-  end
-
-  --button
-  b:SetSize(32, 32)
-
   --icon
-  local icon = _G[name.."Icon"]
+  local icon = b.Icon
   if consolidated then
     if select(1,UnitFactionGroup("player")) == "Alliance" then
       icon:SetTexture(select(3,GetSpellInfo(61573)))
@@ -27,85 +11,38 @@ local function applySkin(b)
     end
   end
 
-  icon:SetTexCoord(0.1,0.9,0.1,0.9)
-  icon:ClearAllPoints()
-  icon:SetPoint("TOPLEFT", b, "TOPLEFT", 2, -2)
-  icon:SetPoint("BOTTOMRIGHT", b, "BOTTOMRIGHT", -2, 2)
-  -- icon:SetDrawLayer("BACKGROUND",-8)
-  b.icon = icon
+  icon:SetTexCoord(0.1,0.94,0.1,0.94)
 
   --border
-  local border = _G[name.."Border"] or b:CreateTexture(name.."Border", "BACKGROUND", nil, -7)
-  border:SetTexture("Interface\\BUTTONS\\WHITE8X8")
-  border:SetTexCoord(0,1,0,1)
-  border:SetDrawLayer("BACKGROUND",-7)
-  if tempenchant then
-    border:SetVertexColor(0.7,0,1)
-  elseif not debuff then
+  local border = b:CreateTexture("AuraBorder", "BACKGROUND", nil, -7)
+  border:SetTexture("Interface\\AddOns\\RillyCleanUI\\media\\textures\\aura-border")
+  border:SetDrawLayer("OVERLAY")
+
+  if b.Border then
+    border:SetVertexColor(b.Border:GetVertexColor())
+    b.Border:Hide()
+  else
     border:SetVertexColor(0,0,0)
   end
-  border:ClearAllPoints()
-  border:SetAllPoints(b)
+
+  border:SetAllPoints(icon)
   b.border = border
 
-  --duration
-  -- b.duration:ClearAllPoints()
-  -- b.duration:SetPoint("TOP",0,0)
-
-  --count
-  -- b.count:ClearAllPoints()
-  -- b.count:SetPoint("CENTER",0,0)
-
-  --shadow
-  local back = CreateFrame("Frame", nil, b, "BackdropTemplate")
-  back:SetPoint("TOPLEFT", b, "TOPLEFT", 0, 0)
-  back:SetPoint("BOTTOMRIGHT", b, "BOTTOMRIGHT", 0, 0)
-  back:SetFrameLevel(b:GetFrameLevel() - 1)
-  back.backdropInfo = RILLY_CLEAN_BUFF_BORDER
-  back:ApplyBackdrop()
-  back:SetBackdropBorderColor(0,0,0,1)
-  b.bg = back
+  b.duration:SetDrawLayer("OVERLAY")
 
   --set button styled variable
   b.rillyClean = true
 end
 
-local function updateAllBuffAnchors()
-    --variables
-    local buttonName  = "BuffButton"
-    local numEnchants = BuffFrame.numEnchants
-    local numBuffs    = BUFF_ACTUAL_DISPLAY
-    local offset      = numEnchants
-    local realIndex, previousButton, aboveButton
+local function updateAuras(self)
+  local auras = self.auraFrames
 
-    --calculate the previous button in case tempenchant or consolidated buff are loaded
-    if BuffFrame.numEnchants > 0 then
-      previousButton = _G["TempEnchant"..numEnchants]
-    end
-
-    if numEnchants > 0 then
-      aboveButton = TempEnchant1
-    end
-
-    --loop on all active buff buttons
-    for index = 1, numBuffs do
-      local button = _G[buttonName..index]
-      if not button then return end
-      if not button.consolidated then
-        --apply skin
-        applySkin(button)
-      end
-    end
-end
-
-local function updateDebuffAnchors(buttonName,index)
-    local button = _G[buttonName..index]
-    if not button then return end
-
+  --loop on all active buff buttons
+  for index, aura in ipairs(auras) do
     --apply skin
-    applySkin(button)
+    applySkin(aura)
+  end
 end
 
-
-hooksecurefunc("BuffFrame_UpdateAllBuffAnchors", updateAllBuffAnchors)
-hooksecurefunc("DebuffButton_UpdateAnchors", updateDebuffAnchors)
+hooksecurefunc(BuffFrame, "UpdateAuraButtons", updateAuras)
+hooksecurefunc(DebuffFrame, "UpdateAuraButtons", updateAuras)
