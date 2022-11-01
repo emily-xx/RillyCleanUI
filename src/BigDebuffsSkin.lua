8cbdd3aefa31b3730e3d7fe93574c2083aca35b8
Emma Chatley <emchatley@gmail.com>
2022-11-01 12:51:54 +0000

    Don't make BigDebuffs square anymore

--- src/BigDebuffsSkin.lua
+++ src/BigDebuffsSkin.lua
@@ -5,19 +5,19 @@ RillyCleanBigDebuffs:SetScript("OnEvent", function()
   if ( not IsAddOnLoaded('BigDebuffs') ) then return end
 
   -- Unit portraits
-  hooksecurefunc(BigDebuffs, 'AttachUnitFrame', function(self, unit)
-    local frameName = 'BigDebuffs' .. unit .. "UnitFrame"
-    local bdbFrame = _G[frameName]
+  -- hooksecurefunc(BigDebuffs, 'AttachUnitFrame', function(self, unit)
+  --   local frameName = 'BigDebuffs' .. unit .. "UnitFrame"
+  --   local bdbFrame = _G[frameName]
 
-    local icon = bdbFrame.icon
+  --   local icon = bdbFrame.icon
 
-    if (icon) then
-      local cooldown = bdbFrame.cooldown or bdbFrame.Cooldown
-      icon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
-      cooldown:SetAllPoints(icon)
-      cooldown:SetSwipeTexture(SQUARE_TEXTURE)
-    end
-  end)
+  --   if (icon) then
+  --     local cooldown = bdbFrame.cooldown or bdbFrame.Cooldown
+  --     icon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
+  --     cooldown:SetAllPoints(icon)
+  --     cooldown:SetSwipeTexture(SQUARE_TEXTURE)
+  --   end
+  -- end)
 
   -- Nameplates
   hooksecurefunc(BigDebuffs, 'NAME_PLATE_UNIT_ADDED', function(self, _, unit)
